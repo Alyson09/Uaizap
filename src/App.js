e02c58f5b322ed058e1ec1bc206b1bd93eebfed2
Alyson <104172831+Alyson09@users.noreply.github.com>
2023-09-19 23:50:24 -0300

    Uaizap-pronto

--- src/App.js
+++ src/App.js
@@ -1,33 +1,101 @@
-import React from 'react';
-import Inputusu from './Component/Inputusu';
-import Inputtext from './Component/Inputtext';
-import Button from './Component/Button';
+import React, { useState, useRef, useEffect } from 'react';
+import Chatpai from './Component/Chatpai';
 import styled from 'styled-components';
-import './App.css';
 
-const Corpo = styled.div`
-  background-color: black;
-  color: white;
+const AppContainer = styled.div`
   display: flex;
-  align-items: center; 
-  width: 40%;
-  margin-left: 30%;
-  margin-right: 15%;
+  flex-direction: column;
+  align-items: center;
   min-height: 100vh;
-` ;
+  background-color: #B0C4DE;
+`;
 
+const ChatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 800px;
+  background-color: #B0C4DE;
+  color: white;
+  flex-grow: 1;
+  padding: 20px;
+  position: relative;
+  height: 100%;
+`;
+
+const MessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: black;
+  padding: 20px; 
+  background-color: #fff;
+  height: 75vh;
+`;
+
+const MessageBubble = styled.div`
+  background-color: #0078FF;
+  color: white;
+  padding: 10px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  align-self: flex-start;
+`;
+
+const Inputpai = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding: 10px;
+  position: sticky;
+  bottom: 0;
+  background-color: #0000CD;
+`;
 
 function App() {
+  const [historicoMensagem, setHistoricoMensagem] = useState([]);
+  const [mensagensApagadas, setMensagensApagadas] = useState([]);
+  const messageContainerRef = useRef(null);
+
+  const atualizaMensagem = (historicoFormatado) => {
+    setHistoricoMensagem([...historicoFormatado]);
+  };
+
+  const apagarMensagem = (index) => {
+    const mensagemApagada = historicoMensagem[index];
+    const novoHistorico = [...historicoMensagem];
+    novoHistorico.splice(index, 1);
+    setHistoricoMensagem(novoHistorico);
+    setMensagensApagadas([...mensagensApagadas, mensagemApagada]);
+  };
+
+  useEffect(() => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
+    }
+  }, [historicoMensagem]);
+
+  const mensagensExibidas = historicoMensagem.filter((mensagem, index) => !mensagensApagadas.includes(mensagem));
+
   return (
-    <Corpo >
-      <div>oi</div>
-      <div>{this.Inputusu.valor}</div>
-      <div className='Inputs'> 
-        <Inputusu />  
-        <Inputtext />
-        <Button /> 
-      </div>   
-    </Corpo>
+    <AppContainer>
+      <ChatContainer>
+        <MessageContainer ref={messageContainerRef}>
+          {mensagensExibidas.map((mensagem, index) => (
+            <MessageBubble
+              key={index}
+              isUsuario={mensagem.isUsuario}
+              style={mensagem.isUsuario ? { alignSelf: 'flex-end', backgroundColor: 'lightblue', color: 'black' } : {}}
+              onDoubleClick={() => apagarMensagem(index)}
+              data-id={index}
+            >
+              {mensagem.mensagem}
+            </MessageBubble>
+          ))}
+        </MessageContainer>
+
+        <Inputpai>
+          <Chatpai onMensagemChange={atualizaMensagem} />
+        </Inputpai>
+      </ChatContainer>
+    </AppContainer>
   );
 }
 
